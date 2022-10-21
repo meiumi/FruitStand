54750238d8f48c72d73cd2dc1edbedb3483528e2
meiumi <meiiuumeii@gmail.com>
2022-10-21 12:17:36 -0500

    FruitStand.py
    Calculator for fruits

---
+++ FruitStand.py
@@ -0,0 +1,16 @@
+def fruitStand():
+    priceOfOrange = 1
+    priceOfPeach = 2
+    priceOfStrawberry = 5
+    priceOfBanana = 3
+    priceOfCherry = 4
+    
+    totalOrange = input("How many oranges?: ")
+    totalPeach = input("How many peaches?: ")
+    totalStrawberry = input("How many strawberries?: ")
+    totalBanana = input("How many bananas?: ")
+    totalCherry = input("How many Cherries?: ")
+    print(int(totalOrange) * priceOfOrange + int(totalPeach) * priceOfPeach + int(totalStrawberry) * priceOfStrawberry + int(totalBanana) * priceOfBanana + int(totalCherry) * priceOfCherry)
+    
+fruitStand();
+    
